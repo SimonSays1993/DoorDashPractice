1bc1015cfe885b373a629abcae6741978001f57c
Simon McNeil <simonmcneil062@gmail.com>
2024-03-18 21:47:34 -0400

    Initial Commit

---
+++ DoorDashPractice/DoorDashPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoorDashPracticeApp.swift
+//  DoorDashPractice
+//
+//  Created by Simon Mcneil on 2024-03-18.
+//
+
+import SwiftUI
+
+@main
+struct DoorDashPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
